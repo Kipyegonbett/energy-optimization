ea4b204d9c11ba5719a7f4577af126a15475a33a
Kipyegonbett <kipyegonbett395@gmail.com>
2025-04-14 14:48:44 +0300

    predictor.py

--- predictor.py
+++ predictor.py
@@ -9,7 +9,7 @@ from sklearn.metrics import mean_absolute_error
 @st.cache
 def load_data():
     # Corrected path format for cross-platform compatibility
-    df = pd.read_csv(r"C:/Users/USER/OneDrive/Desktop/HVAC_DATASET.csv")
+    df = pd.read_csv(r"C:\Users\USER\OneDrive\Desktop\HVAC_DATASET.csv")
     
     # Ensure the 'Timestamp' column is properly converted to datetime format
     df['Timestamp'] = pd.to_datetime(df['Timestamp'], errors='coerce')  # Using 'coerce' to handle invalid date formats
